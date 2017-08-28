af670b9ebef5eb3c511a9bcb9d0db0232eb27fcd
BuritoOfSadness <31352246+BuritoOfSadness@users.noreply.github.com>
2017-08-28 23:33:28 +0200

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def register(request):
+    if request.method == 'post':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/account')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+    return render(request, 'accounts/reg_form.html', args)
